97de2db5ea115256e23a39fac74ed2c16473737a
saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>
2026-08-20 03:03:43 +0530

    One route helath check completed
    Signed-off-by: saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"log"
+
+)
+
 func main() {
-	
+
+    cfg := config{
+		addr: ":8080",
+	}
+
+	app := &application{
+		config: cfg,
+	}
+
+	mux := app.mount()
+	log.Fatal(app.run(mux))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module social-api
 
 go 1.26.4
+
+require github.com/go-chi/chi/v5 v5.3.1 // indirect
